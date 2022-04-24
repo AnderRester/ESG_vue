b2519bfac2fa724a38f95c5af4c962645083a408
anderrester <anderrester@mail.ru>
2022-04-24 22:34:43 +0300

    fix

---
+++ esg_vue/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import { DropdownPlugin } from 'bootstrap-vue';
+import { NavbarPlugin } from 'bootstrap-vue';
+import Popper from 'vue-popperjs';
+import HelloWorld from "@/components/HelloWorld.vue";
+import { mergeData } from "vue-functional-data-merge";
+</script>
+
+<template>
+  <!-- <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header> -->
+  <RouterView />
+</template>
+
+<style>
+/* @import "@/assets/base.css"; */
+@import "@/assets/normalize.css";
+@import "@/assets/main.css";
+@import 'bootstrap/dist/css/bootstrap.css';
+@import 'bootstrap-vue/dist/bootstrap-vue.css';
+@import 'vue-popperjs/dist/vue-popper.css';
+</style>
+
---
+++ esg_vue/src/views/HomeView.vue
@@ -0,0 +1,310 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <div id="main_content_cont">
+      <header class="bg-image w-100 align-items-center">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
+          <a class="navbar-brand" id="logo-image" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24"
+              height="24" fill="currentColor" class="bi bi-cone-striped" viewBox="0 0 16 16">
+              <path
+                d="m9.97 4.88.953 3.811C10.159 8.878 9.14 9 8 9c-1.14 0-2.158-.122-2.923-.309L6.03 4.88C6.635 4.957 7.3 5 8 5s1.365-.043 1.97-.12zm-.245-.978L8.97.88C8.718-.13 7.282-.13 7.03.88L6.275 3.9C6.8 3.965 7.382 4 8 4c.618 0 1.2-.036 1.725-.098zm4.396 8.613a.5.5 0 0 1 .037.96l-6 2a.5.5 0 0 1-.316 0l-6-2a.5.5 0 0 1 .037-.96l2.391-.598.565-2.257c.862.212 1.964.339 3.165.339s2.303-.127 3.165-.339l.565 2.257 2.391.598z" />
+            </svg></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse flex-row-reverse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                    fill="currentColor" class="bi bi-person-badge" viewBox="0 0 16 16">
+                    <path d="M6.5 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                    <path
+                      d="M4.5 0A2.5 2.5 0 0 0 2 2.5V14a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2.5A2.5 2.5 0 0 0 11.5 0h-7zM3 2.5A1.5 1.5 0 0 1 4.5 1h7A1.5 1.5 0 0 1 13 2.5v10.795a4.2 4.2 0 0 0-.776-.492C11.392 12.387 10.063 12 8 12s-3.392.387-4.224.803a4.2 4.2 0 0 0-.776.492V2.5z" />
+                  </svg></a>
+              </li>
+            </ul>
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Contacts</a>
+              </li>
+            </ul>
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Blog</a>
+              </li>
+            </ul>
+            <ul class="navbar-nav ">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Workshop</a>
+              </li>
+            </ul>
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">About</a>
+              </li>
+            </ul>
+
+          </div>
+        </nav>
+        <div class="banner_text_cont text-center">
+          <div class="inner_banner">
+            <h6 class="tagline">We seek future. We are confident about our products.</h6>
+            <div class="banner_main_text_cont">
+              <h2 class="company_name">Eternity Software Group</h2>
+              <h4 class="company_tagline">Choose the right direction</h4>
+            </div>
+            <h6 class="sub_banner_text">Scroll</h6>
+          </div>
+        </div>
+      </header>
+      <section>
+        <div class="founders_cont">
+          <h1>Founders</h1>
+          <div class="card-group">
+            <div class="card">
+              <img class=" card-img-top " src="/images/founders/jUPq7QRYn1M.jpg" alt="Card image cap ">
+              <div class="card-body ">
+                <h5 class="card-title ">Advertiser</h5>
+                <p class="card-text ">This text</p>
+              </div>
+            </div>
+            <div class="card">
+              <img class="card-img-top " src="/images/founders/111.jpg " alt="Card image cap ">
+              <div class="card-body ">
+                <h5 class="card-title ">Developer</h5>
+                <p class="card-text ">This text</p>
+              </div>
+            </div>
+            <div class="card">
+              <img class="card-img-top " src="/images/founders/275854557_348028680716593_7296775242774394012_n.jpg"
+                alt="Card image cap ">
+              <div class="card-body ">
+                <h5 class="card-title ">DB Manager</h5>
+                <p class="card-text ">This text</p>
+              </div>
+            </div>
+          </div>
+          <h4>Those who laid the groundwork<br><span>They are our brain and our proud</span></h4>
+        </div>
+      </section>
+      <section>
+        <div class="workshop_cont">
+          <h1>Our Workshop</h1>
+          <div class="workshop_inner_cont">
+            <div id="carouselExampleIndicators" class="carousel carousel-dark slide" data-bs-ride="carousel">
+              <div class="carousel-indicators" style="margin-top: 30%;">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+                  aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                  aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                  aria-label="Slide 3"></button>
+              </div>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <div class="col">
+                    <div class="card mb-3" style="max-width: 60%;">
+                      <div class="row g-0">
+                        <div class="col-md-5">
+                          <img src="/images/artifacts/vase.png" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-6">
+                          <div class="card-body">
+                            <h5 class="card-title">Ancient Vase</h5>
+                            <p class="card-text">This is an ancient vase, found near Balti city.</p>
+                            <p class="card-text">It has been found 20 years ago, near to the Reutel area.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            <p class="text-center"><button class="btn btn-primary" id="buttonAboutTest"
+                                type="button">About</button></p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <div class="col">
+                    <div class="card mb-3" style="max-width: 60%;">
+                      <div class="row g-0">
+                        <div class="col-md-5">
+                          <img src="/images/artifacts/plate.png" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-6">
+                          <div class="card-body">
+                            <h5 class="card-title">Ancient Plate</h5>
+                            <p class="card-text">This is an ancient plate, found near Balti city.</p>
+                            <p class="card-text">It has been found 40 years ago, near to the Sangerei area.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            <p class="text-center"><button class="btn btn-primary" id="buttonAboutTest"
+                                type="button">About</button></p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <div class="col">
+                    <div class="card mb-3" style="max-width: 60%;">
+                      <div class="row g-0">
+                        <div class="col-md-5">
+                          <img src="/images/artifacts/scythe.png" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-6">
+                          <div class="card-body">
+                            <h5 class="card-title">Ancient Scythe</h5>
+                            <p class="card-text">This is an ancient scythe, found near Balti city.</p>
+                            <p class="card-text">It has been found 60 years ago, near to the Reutel area.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            <p class="text-center"><button class="btn btn-primary" id="buttonAboutTest"
+                                type="button">About</button></p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+          </div>
+          <h4>Check our latest works
+            <br><span>Let us know, if you are interested</span>
+          </h4>
+        </div>
+      </section>
+      <section>
+        <div class="project_description">
+          <h1>Projects</h1>
+          <div class="project_inner row col-md-12 justify-content-around">
+            <article class="col project_description_article">At the moment our main project is Virtual Museum of World
+              History. Check this out: large virtual halls, tens artiffacts, interesting and revolutionazing mechanics,
+              in addition it is compatible with VR helmet of any types.
+              <span class="project_description_article_optimization">A lot of artifacts from a lot of countries.
+                Everyweek updates reflected in our blog and social media. You have full acces to the progress and
+                already developed things. Imagine fragments of territories, realistic landscapes,
+                buildings, pictures of the past and present. Possible we will add modren artifacts, mechanisms and piece
+                of arts. It will be interesting expirience as for us also for you.</span>
+            </article>
+            <div class="col-md-3 d-flex"></div>
+            <div class="project_description_preview col"></div>
+          </div>
+          <h4 class="project_description_h4">Our development <span class="project_description_span"><br>Be near us, near
+              the future</span></h4>
+        </div>
+      </section>
+
+
+      <!-- Footer -->
+      <footer class="text-center text-lg-start bg-light text-muted d-grid">
+        <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+          <div class="me-5 d-none d-lg-block">
+            <span>Get connected with us on social networks:</span>
+          </div>
+          <div>
+            <a href="" class="me-4 text-reset">
+              <i class=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                  class="bi bi-facebook" viewBox="0 0 16 16">
+                  <path
+                    d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
+                </svg></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+              <i class="fab fa-twitter"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                  fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+                  <path
+                    d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
+                </svg></i>
+            </a>
+            <a href="https://www.instagram.com/eternity.software.group/" class="me-4 text-reset">
+              <i class="fab fa-instagram"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                  fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
+                  <path
+                    d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z" />
+                </svg></i>
+            </a>
+          </div>
+        </section>
+        <section class="">
+          <div class="container text-center text-md-start mt-5">
+
+            <div class="row mt-3">
+
+              <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+                <h6 class="text-uppercase fw-bold mb-4">
+                  <i class="fas fa-gem"></i>Eternity Software Group
+                </h6>
+                <p>
+                  Company was found with goal to change the word.
+                </p>
+              </div>
+              <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                <h6 class="text-uppercase fw-bold mb-4">
+                  Products
+                </h6>
+                <p>
+                  <a href="#!" class="text-reset">Virtual Museum of World History</a>
+                </p>
+              </div>
+              <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+                <h6 class="text-uppercase fw-bold mb-4">
+                  Useful links
+                </h6>
+                <p>
+                  <a href="https://usarb.md/" class="text-reset">USARB</a>
+                </p>
+                <p>
+                  <a href="#!" class="text-reset">Suggestions</a>
+                </p>
+                <p>
+                  <a href="#!" class="text-reset">Orders</a>
+                </p>
+                <p>
+                  <a href="#!" class="text-reset">Help</a>
+                </p>
+              </div>
+
+
+
+              <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+                <h6 class="text-uppercase fw-bold mb-4">
+                  Contact
+                </h6>
+                <p><i class="fas fa-home"></i> Balti, MD</p>
+                <p>
+                  <i class="fas fa-envelope"></i> eternity.software.group@gmail.com
+                </p>
+                <!-- <p><i class="fas fa-phone"></i> + 01 234 567 88</p> -->
+              </div>
+
+            </div>
+
+          </div>
+        </section>
+
+
+
+        <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+          © 2021 Copyright:
+          <a class="text-reset fw-bold" href="https://eternity-software-group.web.app/">Eternity Software Group</a>
+        </div>
+
+      </footer>
+    </div>
+  </main>
+</template>
